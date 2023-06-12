2a96e623c3499bb29e8df6bb637a7ccf61f96e1c
Sérgio Gabriel <nihhiu17@gmail.com>
2023-06-12 23:58:19 +0100

    aesthetics and sounds update

--- Assets/Scripts/PlayerWalk.cs
+++ Assets/Scripts/PlayerWalk.cs
@@ -35,9 +35,13 @@ public class PlayerWalk : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //check if the player is touching an object of the type ground
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        //check if the player is touching an object of the type wall
         isWall = Physics.CheckSphere(groundCheck.position, groundDistance, wallMask);
 
+        //if the player is in the floor, then reset all changeable variables
         if(isGrounded && velocity.y < 0)
         {
             velocity.y = -2f;
@@ -45,11 +49,14 @@ public class PlayerWalk : MonoBehaviour
             freeFall = transform.position.y;
         }
 
+        //movement coordenates
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
+        //move to the right depending on X / move forward depending on Z
         Vector3 move = transform.right * x + transform.forward * z;
 
+        //if sprintKey is being held, then player will move at sprinting speed, otherwise, at walking speed
         if(Input.GetKey(sprintKey))
         {
             controller.Move(move * playerSprintSpeed * Time.deltaTime);
@@ -58,9 +65,11 @@ public class PlayerWalk : MonoBehaviour
             controller.Move(move * playerWalkSpeed * Time.deltaTime);
         }
 
+        //if the player jumps, and the player is grounded or in a wall, and they can jump in a wall, it will jump
         if(Input.GetKey(jumpKey) && (isGrounded || isWall) && canJumpWall)
         {
             velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            //after jumping one in a wall, they will no longer be able to, unless they touch the ground again to reset
             if(isWall)
             {
                 canJumpWall = false;
@@ -69,6 +78,7 @@ public class PlayerWalk : MonoBehaviour
 
         velocity.y  += gravity * Time.deltaTime;
 
+        //will apply the jumping move
         controller.Move(velocity * Time.deltaTime);
     }
 
